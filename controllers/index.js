e507f2ff32ac747e7ebb76aab7444d0e3148a6df
Amir Shariat <amirshariat1987@gmail.com>
2022-03-22 11:57:50 -0600

    home page done

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes.js");
 
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
 
 // This is so if we make a request to any endpoint that doesn't exist, we'll receive a 404 error indicating we have requested an incorrect resource, another RESTful API practice.
 router.use((req, res) => {
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers/");
 const sequelize = require("./config/connection");
 const path = require("path");
 
@@ -13,6 +13,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes. Since we set up the routes the way we did, we don't have to worry about importing multiple files for different endpoints. The router instance in routes/index.js collected everything for us and packaged them up for server.js to use.
 app.use(routes);
+
+// Set up Handlebars.js as my app's template engine of choice.
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // turn on connection to db and server. We use the sequelize.sync() method to establish the connection to the database. The "sync" part means that this is Sequelize taking the models and connecting them to associated database tables. If it doesn't find a table, it'll create it for you! The other thing to notice is the use of {force: false} in the .sync() method. This doesn't have to be included, but if it were set to true, it would drop and re-create all of the database tables on startup. This is great for when we make changes to the Sequelize models, as the database would need a way to understand that something has changed. This definition performs similarly to DROP TABLE IF EXISTS, which was used previously. This allows the table to be overwritten and re-created. By forcing the sync method to true, we will make the tables re-create if there are any association changes.
 sequelize.sync({ force: false }).then(() => {
